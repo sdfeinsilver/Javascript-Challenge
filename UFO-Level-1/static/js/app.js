4b8ab9a974d7ae0422d22f981ce3a308b27eb064
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-02-28 16:37:49 -0600

    Looked at sample code from tutor

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -11,31 +11,34 @@ let tbody = d3.select("tbody");
 function clickButton() {
     let myDate = d3.select("#datetime").property("value");
     console.log(myDate);
+
+    if(myDate) {
+        filteredRows = tableData.filter(row => row.datetime === myDate);
+    }
+    else {
+        filteredRows = tableData;
+    }
+
+
+    // Wipe out any row data
+    tbody.html("");
+
+
+    //forEach Statement
+    filteredRows.forEach((row_data) => {
+    let row = tbody.append("tr");
+
+        Object.values(row_data).forEach((value) => {
+            let cell = row.append("td");
+            cell.text(value);
+        }
+        );
+    });
 }
 
-// // Select the button
-// let button = d3.select("#filter-btn");
+//Button click
+d3.selectAll("#filter-btn").on("click", clickButton);
 
-// // Select the form
-// //let form = d3.select("#ufo-table");
-
-// //Create event handlers
-// button.on("click", runEnter);
-// //form.on("submit", runEnter);
-
-// // Complete the event handler function for the form
-// function runEnter(event) {
-
-//     //Prevent the page from refreshing
-//     event.preventDefault();
-
-//     //Select the inpout element and get the raw HTMLnode
-//     let inputElement = d3.select("datetime");
-
-//     //Inspect the data in the console
-//     console.log(inputElement);
-// }
-
-
-
+// First time browser loads the page
+btnClick();
 
