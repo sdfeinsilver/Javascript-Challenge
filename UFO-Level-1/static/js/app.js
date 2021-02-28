c07fc868f085c8d115e3552069e3b607384e7fcb
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-02-28 16:18:15 -0600

    Messing around with function for part 1

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,30 +1,41 @@
 // Assign the data from 'data.js' to a descriptive value
 const tableData = data;
 
-// Select the button
-let button = d3.select("#filter-btn");
+// Log to check that the data is usable
+console.log(tableData);
 
-// Select the form
-//let form = d3.select("#ufo-table");
+// Get a reference to the table body
+let tbody = d3.select("tbody");
 
-//Create event handlers
-button.on("click", runEnter);
-//form.on("submit", runEnter);
-
-// Complete the event handler function for the form
-function runEnter(event) {
-
-    //Prevent the page from refreshing
-    event.preventDefault();
-
-    //Select hte inpout element and get the raw HTMLnode
-    let inputElement = d3.select("")
+// Create function:
+function clickButton() {
+    let myDate = d3.select("#datetime").property("value");
+    console.log(myDate);
 }
 
+// // Select the button
+// let button = d3.select("#filter-btn");
+
+// // Select the form
+// //let form = d3.select("#ufo-table");
+
+// //Create event handlers
+// button.on("click", runEnter);
+// //form.on("submit", runEnter);
+
+// // Complete the event handler function for the form
+// function runEnter(event) {
+
+//     //Prevent the page from refreshing
+//     event.preventDefault();
+
+//     //Select the inpout element and get the raw HTMLnode
+//     let inputElement = d3.select("datetime");
+
+//     //Inspect the data in the console
+//     console.log(inputElement);
+// }
 
 
 
 
-function btnClick() {
-    const myDate = d3.select("#datetime").property("value");
-}.
