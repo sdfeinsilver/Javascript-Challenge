5a7f984d7f615c78d1789b1a5dd0d94f446c68f1
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-02-28 14:23:23 -0600

    Beginning code

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,11 +1,29 @@
-// from data.js
+// Assign the data from 'data.js' to a descriptive value
 const tableData = data;
 
-console.log("tableData")
-console.log(tableData)
+// Select the button
+let button = d3.select("#filter-btn");
 
-// YOUR CODE HERE!
-const tbody = d3.select("tbody");
+// Select the form
+//let form = d3.select("#ufo-table");
+
+//Create event handlers
+button.on("click", runEnter);
+//form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter(event) {
+
+    //Prevent the page from refreshing
+    event.preventDefault();
+
+    //Select hte inpout element and get the raw HTMLnode
+    let inputElement = d3.select("")
+}
+
+
+
+
 
 function btnClick() {
     const myDate = d3.select("#datetime").property("value");
