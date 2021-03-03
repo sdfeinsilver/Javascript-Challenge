1224a68446460085b12c60b73a05531041f466d9
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-03-03 17:21:26 -0600

    Working on function for button click

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,34 +1,31 @@
 //Scope: Javascript code that allows the user to enter a date in website, and output will be UFO data that matches the date
 
-//1. Check that you have access to data in javascript file
-console.log(data);
-//Check: Data successfully logged in console!
-
-//2. Assign the data to a descriptive variable
+//1. Assign the data to a descriptive variable
 let ufoSightings = data
 
-//3. Select the button
+//2. Select the button
 let button = d3.select("#filter-btn");
 
-//4. Get a handle on the table body
+//3. Get a handle on the table body
 let tbody = d3.select("tbody");
 
-//5. Use forEach loop to loop through data and print each object to console
-data.forEach(function (ufoSighting) {
-    console.log(ufoSighting);
-});
-//Check: Individual objects were successfully logged in console
-
-//6. Create event handler for button 
+//4. Create event handler
 button.on("click", filterTable);
 
-//7. Create function that will filter table by date
-function filterTable(date) {
-
+//5. Complete event handler function for click of button
+function filterTable(event) {
     // Prevent the page from refreshing
     event.preventDefault();
 
+    //Select the user date input and get the raw HTML code
+    let userDate = d3.select("#datetime").property("value");
 
-
-    data.forEach(function ())
-}
+    //if statement to filter out ufoSightings table based on userDate
+    if (userDate = ufoSightings.datetime) {
+        let row = tbody.append("tr");
+        let cell = row.append("td");
+        Object.values(ufoSightings).forEach((value) => {
+            let cell = row.append("td");
+            cell.text(value);
+        }
+    
