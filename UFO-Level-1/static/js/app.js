3434421f6e8755071801d0b75df1314adfc18721
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-03-03 13:54:12 -0600

    Starting Code Fresh

---
+++ UFO-Level-1/static/js/app.js
@@ -0,0 +1,11 @@
+//Scope: Javascript code that allows the user to enter a date in website, and output will be UFO data that matches the date
+
+//1. Check that you have access to data in javascript file
+console.log(data);
+//Data successfully renders in console!
+
+//2. Get a handle on the table body
+let tbody = d3.select("tbody");
+
+
+
