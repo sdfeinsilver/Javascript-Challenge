d9e0123e830ced0c622c708e8405173c4cb2d1da
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-03-03 14:20:13 -0600

    Working on function

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -2,10 +2,16 @@
 
 //1. Check that you have access to data in javascript file
 console.log(data);
-//Data successfully renders in console!
+    //Check: Data successfully logged in console!
 
 //2. Get a handle on the table body
 let tbody = d3.select("tbody");
+
+//3. use forEach loop to loop through data and print each object to console
+data.forEach(function (ufoSighting) {
+    console.log(ufoSighting);
+});
+    //Check: Individual objects were successfully logged in console
 
 
 
