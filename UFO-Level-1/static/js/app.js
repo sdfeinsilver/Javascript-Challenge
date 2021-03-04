ae9d4f387636286a7787d54f128fb83239dc84c4
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-03-03 22:19:36 -0600

    Last commit

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,31 +1,42 @@
-//Scope: Javascript code that allows the user to enter a date in website, and output will be UFO data that matches the date
-
 //1. Assign the data to a descriptive variable
-let ufoSightings = data
+let ufoSightings = data;
 
-//2. Select the button
-let button = d3.select("#filter-btn");
+//2. Log to check that the data is usable
+console.log(ufoSightings);
 
-//3. Get a handle on the table body
-let tbody = d3.select("tbody");
+//3. Get a reference to the table body
+tbody = d3.select("tbody");
 
-//4. Create event handler
-button.on("click", filterTable);
-
-//5. Complete event handler function for click of button
-function filterTable(event) {
-    // Prevent the page from refreshing
-    event.preventDefault();
-
-    //Select the user date input and get the raw HTML code
+//4. Create callback function for event handler
+function filterTable() {
     let userDate = d3.select("#datetime").property("value");
 
-    //if statement to filter out ufoSightings table based on userDate
-    if (userDate = ufoSightings.datetime) {
+    if(userDate) {
+        filteredRows = ufoSightings.filter(row => row.datetime === userDate);
+    }
+    else {
+        filteredRows = ufoSightings;
+    }
+
+    //Deletes row data
+    tbody.html("");
+
+    filteredRows.forEach((row_data) => {
         let row = tbody.append("tr");
-        let cell = row.append("td");
-        Object.values(ufoSightings).forEach((value) => {
-            let cell = row.append("td");
-            cell.text(value);
-        }
-    
+
+            Object.values(row_data).forEach((value) => {
+                let cell = row.append("td");
+                cell.text(value);
+            }
+            );
+    });
+}
+
+//5. Establish event handler
+button = d3.select("#filter-btn");
+
+button.on("click", filterTable);
+
+//6. First time browser loads the page
+btnClick();
+
